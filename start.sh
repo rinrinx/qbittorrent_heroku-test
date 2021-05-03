57473b06bc001a83becdf97109e2444e67569549
winkxx <68945988+winkxx@users.noreply.github.com>
2021-05-03 13:31:18 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,11 +15,6 @@ chown 0777 -R /config \
 chmod 0777 /upload/ -R
 
 nohup yes "" | qbittorrent-nox --webui-port=$PORT  --profile=/config  &
-python3 /upload/config.py
 python3 /upload/dingshi.py
-python3 /upload/check.py
 python3 /upload/first.py
-python3 /upload/link.py
-python3 /upload/mover.py
-python3 /upload/qb.py
-python3 /upload/rclone.py
+
