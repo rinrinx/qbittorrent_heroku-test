d1590e2f9012c184bdb2e07f739501891bbfd1ec
winkxx <68945988+winkxx@users.noreply.github.com>
2021-06-05 12:30:41 +0800

    Update start.sh

--- root/start.sh
+++ root/start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 sudo /usr/sbin/service cron start
 chmod 0777 /upload/ -R
-git clone https://github.com/winkxx/rclone
-chmod 0777 /rclone/ -R
-cp -f rclone/rclone.conf config/rclone
 python3 /upload/config.py
 python3 /upload/dingshi.py
 yes "" | qbittorrent-nox --webui-port=$PORT  --profile=/config
