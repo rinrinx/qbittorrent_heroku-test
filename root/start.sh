b4d39c8d538b2bd12bca487882c238e65e2e1843
winkxx <68945988+winkxx@users.noreply.github.com>
2022-03-17 19:09:42 +0800

    Update start.sh

--- root/start.sh
+++ root/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
+cd root
+mkdir /root/config
+chmod 0777 /root/config
 # 检查config配置文件，并创建
 if [ ! -e "/config/qBittorrent/config/qBittorrent.conf" ] ;  then 
 mkdir -p /config/qBittorrent/config/
@@ -8,16 +10,21 @@ cp /usr/local/qbittorrent/defaults/qBittorrent.conf  /config/qBittorrent/config/
 fi
 
 # permissions
-chown 0777 -R /config \
-    /downloads
-
+mkdir /root/downloads
+chmod 0777 /root/downloads
+cd /root
 git clone https://github.com/winkxx/qbittorrent_heroku
-mkdir /upload/
-mv /qbittorrent_heroku/upload/* /upload/
+mkdir /root/qb
+mkdir /root/upload/
+chmod 0777 /root/qb
+chmod 0777 /root/upload
 chmod 0777 /qbittorrent_heroku/upload/ -R
+chmod 0777 /qbittorrent_heroku/root/ -R
+mv /qbittorrent_heroku/upload/* /root/upload/
+mv /qbittorrent_heroku/root/* /root/qb/
 #rm -rf /qbittorrent_heroku
 
 
-nohup yes "" | qbittorrent-nox --webui-port=$PORT  --profile=/config  &
-python3 /qbittorrent_heroku/upload/config.py
-python3 /qbittorrent_heroku/upload/dingshi.py
+nohup yes "" | /root/config/qbittorrent-nox --webui-port=$PORT  --profile=/root/config  &
+python3 /root/qbittorrent_heroku/upload/config.py
+python3 /root/qbittorrent_heroku/upload/dingshi.py
