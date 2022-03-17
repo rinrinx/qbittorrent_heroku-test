148b1202d0f052251edd3e909e9732d75a5a54db
winkxx <68945988+winkxx@users.noreply.github.com>
2022-03-17 19:01:46 +0800

    Update start.sh

--- root/start.sh
+++ root/start.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-sudo /usr/sbin/service cron start
-chmod 0777 /upload/ -R
-python3 /upload/config.py
-yes "" | qbittorrent-nox --webui-port=$PORT  --profile=/config
+
+
+# 检查config配置文件，并创建
+if [ ! -e "/config/qBittorrent/config/qBittorrent.conf" ] ;  then 
+mkdir -p /config/qBittorrent/config/
+cp /usr/local/qbittorrent/defaults/qBittorrent.conf  /config/qBittorrent/config/qBittorrent.conf
+fi
+
+# permissions
+chown 0777 -R /config \
+    /downloads
+
+git clone https://github.com/winkxx/qbittorrent_heroku
+mkdir /upload/
+mv /qbittorrent_heroku/upload/* /upload/
+chmod 0777 /qbittorrent_heroku/upload/ -R
+#rm -rf /qbittorrent_heroku
+
+
+nohup yes "" | qbittorrent-nox --webui-port=$PORT  --profile=/config  &
+python3 /qbittorrent_heroku/upload/config.py
+python3 /qbittorrent_heroku/upload/dingshi.py
